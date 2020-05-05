95f980c2e5d356b3256dc7d829e136b1968ea57f
guyschwartzberg-student <schwartzberg@mail.tau.ac.il>
2020-05-05 19:35:15 +0300

    Removed unused websites

--- recipe_scrapers/__init__.py
+++ recipe_scrapers/__init__.py
@@ -13,51 +13,10 @@ from .cookstr import Cookstr
 from .copykat import CopyKat
 from .delish import Delish
 from .epicurious import Epicurious
-from .finedininglovers import FineDiningLovers
 from .food import Food
 from .foodnetwork import FoodNetwork
 from .foodrepublic import FoodRepublic
-from .giallozafferano import GialloZafferano
-from .gonnawantseconds import GonnaWantSeconds
-from .greatbritishchefs import GreatBritishChefs
-from .heinzbrasil import HeinzBrasil
-from .hellofresh import HelloFresh
-from .hundredandonecookbooks import HundredAndOneCookbooks
-from .ig import IG
-from .inspiralized import Inspiralized
-from .jamieoliver import JamieOliver
-from .justbento import JustBento
-from .kennymcgovern import KennyMcGovern
-from .kitchn import Kitchn
-from .marmiton import Marmiton
-from .matprat import Matprat
-from .mindmegette import Mindmegette
-from .misya import Misya
-from .mybakingaddiction import MyBakingAddiction
-from .nihhealthyeating import NIHHealthyEating
-from .panelinha import Panelinha
-from .paninihappy import PaniniHappy
-from .przepisy import Przepisy
-from .realsimple import RealSimple
-from .seriouseats import SeriousEats
-from .simplyrecipes import SimplyRecipes
-from .southernliving import SouthernLiving
-from .steamykitchen import SteamyKitchen
-from .tastesoflizzyt import TastesOfLizzyT
 from .tasty import Tasty
-from .tastykitchen import TastyKitchen
-from .thehappyfoodie import TheHappyFoodie
-from .thepioneerwoman import ThePioneerWoman
-from .thespruceeats import TheSpruceEats
-from .thevintagemixer import TheVintageMixer
-from .tineno import TineNo
-from .tudogostoso import TudoGostoso
-from .twopeasandtheirpod import TwoPeasAndTheirPod
-from .whatsgabycooking import WhatsGabyCooking
-from .wikicookbook import WikiCookbook
-from .yummly import Yummly
-from .geniuskitchen import GeniusKitchen
-
 SCRAPERS = {
     AllRecipes.host(): AllRecipes,
     BBCFood.host(): BBCFood,
@@ -71,50 +30,10 @@ SCRAPERS = {
     CopyKat.host(): CopyKat,
     Delish.host(): Delish,
     Epicurious.host(): Epicurious,
-    FineDiningLovers.host(): FineDiningLovers,
     Food.host(): Food,
     FoodNetwork.host(): FoodNetwork,
     FoodRepublic.host(): FoodRepublic,
-    GialloZafferano.host(): GialloZafferano,
-    GonnaWantSeconds.host(): GonnaWantSeconds,
-    GreatBritishChefs.host(): GreatBritishChefs,
-    HeinzBrasil.host(): HeinzBrasil,
-    HelloFresh.host(): HelloFresh,
-    HelloFresh.host(domain='co.uk'): HelloFresh,
-    HundredAndOneCookbooks.host(): HundredAndOneCookbooks,
-    IG.host(): IG,
-    Inspiralized.host(): Inspiralized,
-    JamieOliver.host(): JamieOliver,
-    JustBento.host(): JustBento,
-    Kitchn.host(): Kitchn,
-    Marmiton.host(): Marmiton,
-    Matprat.host(): Matprat,
-    Mindmegette.host(): Mindmegette,
-    Misya.host(): Misya,
-    MyBakingAddiction.host(): MyBakingAddiction,
-    NIHHealthyEating.host(): NIHHealthyEating,
-    Panelinha.host(): Panelinha,
-    PaniniHappy.host(): PaniniHappy,
-    Przepisy.host(): Przepisy,
-    RealSimple.host(): RealSimple,
-    SeriousEats.host(): SeriousEats,
-    SimplyRecipes.host(): SimplyRecipes,
-    SouthernLiving.host(): SouthernLiving,
-    SteamyKitchen.host(): SteamyKitchen,
-    TastesOfLizzyT.host(): TastesOfLizzyT,
-    Tasty.host(): Tasty,
-    TastyKitchen.host(): TastyKitchen,
-    TheHappyFoodie.host(): TheHappyFoodie,
-    ThePioneerWoman.host(): ThePioneerWoman,
-    TheSpruceEats.host(): TheSpruceEats,
-    TheVintageMixer.host(): TheVintageMixer,
-    TineNo.host(): TineNo,
-    TudoGostoso.host(): TudoGostoso,
-    TwoPeasAndTheirPod.host(): TwoPeasAndTheirPod,
-    WhatsGabyCooking.host(): WhatsGabyCooking,
-    WikiCookbook.host(): WikiCookbook,
-    Yummly.host(): Yummly,
-    GeniusKitchen.host(): GeniusKitchen,
+    Tasty.host(): Tasty
 }
 
 
